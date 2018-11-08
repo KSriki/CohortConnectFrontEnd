e466498b8255830b964d3dfadb2402d103c6ea04
drubaby <peryton6@gmail.com>
2018-11-08 15:29:36 -0500

    minor fixes

--- src/components/UserCard.js
+++ src/components/UserCard.js
@@ -2,17 +2,26 @@ import React from "react";
 import { Card, Icon, Image } from "semantic-ui-react";
 
 export default class UserCard extends React.Component {
+
+
+handleStatus = (e) => {
+  setTimeout(function (e) {
+    console.log(e.target.value), 500
+  }
+}
+
+
   render() {
     let user = this.props.user;
     return (
       <Card className="ui card">
         <Image src={user.avatar_url} alt="broken" />
         <Card.Content>
-          <Card.Header>{user.name}</Card.Header>
+          <Card.Header>{user.name ? user.name : user.login}</Card.Header>
           <Card.Meta>
             <span className="date">{user.github_identifier}</span>
           </Card.Meta>
-          <Card.Description>{user.bio}</Card.Description>
+          <Card.Description>{user.bio ? user.bio : "Flatiron Cohort - 082718"}</Card.Description>
         </Card.Content>
         <Card.Content>
           <p>
@@ -23,7 +32,7 @@ export default class UserCard extends React.Component {
         <Card.Content className="extra content">
           <div className="ui large transparent left icon input">
             <i className="heart outline icon" />
-            <input type="text" placeholder="Add Comment..." />
+            <input onChange={this.handleStatus} type="text" placeholder="Set status..." />
           </div>
         </Card.Content>
       </Card>
