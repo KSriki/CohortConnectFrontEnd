bda153b71589c5fee2fca8e324b77d584141759f
drubaby <peryton6@gmail.com>
2018-11-08 14:25:44 -0500

    ui improvements, added comment field

--- src/components/UserCard.js
+++ src/components/UserCard.js
@@ -1,30 +1,32 @@
-import React from 'react'
-import {Card, Icon, Image} from 'semantic-ui-react'
+import React from "react";
+import { Card, Icon, Image } from "semantic-ui-react";
 
 export default class UserCard extends React.Component {
-
-    render() {
-
-        let user = this.props.user;
-        return (
-            <Card>
-                <Image src={user.avatar_url} alt="broken"/>
-                <Card.Content>
-                    <Card.Header>{user.name}</Card.Header>
-                    <Card.Meta>
-                        <span className='date'>{user.login}</span>
-                    </Card.Meta>
-                    <Card.Description>{user.bio}</Card.Description>
-                </Card.Content>
-                <Card.Content extra="extra">
-                    <a>
-                        <Icon name='user'/>
-                        {user.github_identifier}
-                    </a>
-                </Card.Content>
-            </Card>
-        )
-
-    }
-
+  render() {
+    let user = this.props.user;
+    return (
+      <Card className="ui card">
+        <Image src={user.avatar_url} alt="broken" />
+        <Card.Content>
+          <Card.Header>{user.name}</Card.Header>
+          <Card.Meta>
+            <span className="date">{user.github_identifier}</span>
+          </Card.Meta>
+          <Card.Description>{user.bio}</Card.Description>
+        </Card.Content>
+        <Card.Content>
+          <p>
+            <Icon name="user" />
+            {user.login}
+          </p>
+        </Card.Content>
+        <Card.Content className="extra content">
+          <div className="ui large transparent left icon input">
+            <i className="heart outline icon" />
+            <input type="text" placeholder="Add Comment..." />
+          </div>
+        </Card.Content>
+      </Card>
+    );
+  }
 }
--- src/containers/ConnectContainer.js
+++ src/containers/ConnectContainer.js
@@ -1,45 +1,35 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
-import UserCard from '../components/UserCard'
-import { Grid, Image } from 'semantic-ui-react'
+import UserCard from "../components/UserCard";
+import { Grid } from "semantic-ui-react";
 
 export default class ConnectContainer extends Component {
+  constructor() {
+    super();
+    this.state = {
+      currentUser: 0,
+      users: []
+    };
+  }
+  componentDidMount() {
+    fetch(`http://localhost:3000/users/`)
+      .then(resp => resp.json())
+      .then(json => this.setState({ users: json }));
+  }
 
-    constructor(){
-        super();
-        this.state = {
-            currentUser: 0,
-            users: []
-        }
-    }
-    componentDidMount(){
-
-        fetch(`http://localhost:3000/users/`)
-        .then(resp => resp.json())
-        .then(json => this.setState({users: json}));
-
-    }
-
-    render(){
-
-        return (
-        <Grid columns={3} divided>
-            <Grid.Row>
-
-            {this.state.users.map(user => {return (<Grid.Column><UserCard key={user.id} user={user}/></Grid.Column>)} )}
-
-
-            </Grid.Row>
-            </Grid>)
-
-
-
-
-
-
-    }
-
-
-
-
+  render() {
+    return (
+      <Grid columns={3} >
+        <Grid.Row>
+          {this.state.users.map(user => {
+            return (
+              <Grid.Column key={user.id}>
+                <UserCard key={user.id} user={user} />
+              </Grid.Column>
+            );
+          })}
+        </Grid.Row>
+      </Grid>
+    );
+  }
 }
