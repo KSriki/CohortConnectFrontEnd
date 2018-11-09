6c1f63d190a52b5b3dc6afae106341edd003a0a4
Srikant Kumar Kalaputapu <skalaputapu@gmail.com>
2018-11-09 10:24:00 -0500

    Conditional rendering of routes

--- src/containers/DetailsContainer.js
+++ src/containers/DetailsContainer.js
@@ -11,13 +11,13 @@ import UserDetails from '../components/UserDetails'
 
 
      render(){
-        
+
          //full details for everyone
          //or details for one person
          //// <Route exact path='/details' component={FullRoster}/>
          return (
              <Switch>
-             <Route path='/details/:number' component={() => <UserDetails users={this.props.users} />}/>
+             <Route path='/details/:number' component={(props) => <UserDetails {...props} users={this.props.users} />}/>
              </Switch>
          );
 
