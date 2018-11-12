529620ad83a27989e5462f835079354a94c1035b
drubaby <peryton6@gmail.com>
2018-11-12 17:21:19 -0500

    minor style fix

--- src/components/Event.js
+++ src/components/Event.js
@@ -1,12 +1,12 @@
 import React from "react";
 
-import { List, Item } from "semantic-ui-react";
+import { Item } from "semantic-ui-react";
 import { getDifference } from "../containers/ConnectContainer";
 
 export default class Event extends React.Component {
   render() {
     let gitEvent = this.props.eventObj;
-    let repo_url = "http://github.com";
+    // let repo_url = "http://github.com";
     let eventType = "Event Type";
     let repoName = "Repo Name";
     let ago = new Date().toLocaleDateString();
@@ -18,7 +18,7 @@ export default class Event extends React.Component {
       }
       if (gitEvent.repo) {
         repoName = gitEvent.repo.name;
-        repo_url = "https://github.com/" + repoName;
+        // repo_url = "https://github.com/" + repoName;
       }
     }
 
